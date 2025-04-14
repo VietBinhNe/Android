8d6dce9584de518ba28800077246c02afb9769ec
Viet Binh <nguyenducvietbinh@gmail.com>
2025-04-14 16:17:10 +0700

    add : week 6 - activities

---
+++ Week6_LoginUI/app/src/main/java/com/example/week6_loginui/SuccessActivity.java
@@ -0,0 +1,29 @@
+package com.example.week6_loginui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SuccessActivity extends AppCompatActivity {
+
+    private Button logoutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+
+        logoutButton = findViewById(R.id.logoutButton);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SuccessActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
